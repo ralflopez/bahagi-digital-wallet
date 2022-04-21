977994d4277fadbeb1b40749e66e52ef0a176259
Ralf <ralflopez3@gmail.com>
2022-04-21 14:25:36 +0800

    :necktive: confirm cash in from paymongo webhook

--- src/paymongo/paymongo.controller.ts
+++ src/paymongo/paymongo.controller.ts
@@ -1,7 +1,5 @@
 import { Body, Controller, Post, Session } from '@nestjs/common';
-import { ExternalFundTransferMethod } from 'src/external-fund-transfers/enums/method.enum';
 import { FundTransferStatus } from 'src/fund-transfers/enums/status.enum';
-import { FundTransferType } from 'src/fund-transfers/enums/type.enum';
 import { ExternalFundTransfersService } from '../external-fund-transfers/external-fund-transfers.service';
 import { WebhookEventDto } from './dto/webhook-event.dto';
 
@@ -19,36 +17,20 @@ export class PaymongoController {
     const user = session.user || { id: '12a9b624-3202-463b-9ebc-8fd5e80ee9e4' };
     if (!user) throw new Error('Unauthorized');
 
-    console.log(webhookEventDto);
-    console.log('*********************************************');
-
     const paymentIntentId =
       webhookEventDto?.data?.attributes?.data?.attributes?.payment_intent_id;
-    console.log(paymentIntentId);
 
-    // await this.externalFundTransferService.updateCashInStatus(
-    //   paymentIntentId,
-    //   FundTransferStatus.SUCCESS,
-    // );
+    const isPaymentFailed =
+      webhookEventDto.data.attributes.type === 'payment.failed';
 
-    return 'null';
+    const externalFundTransfer =
+      await this.externalFundTransferService.updateCashInStatus(
+        paymentIntentId,
+        isPaymentFailed
+          ? FundTransferStatus.FAILED
+          : FundTransferStatus.SUCCESS,
+      );
 
-    // const externalFundTransfer = await this.externalFundTransferService.create(
-    //   webhookEventDto.data.id,
-    //   ExternalFundTransferMethod.CASH_IN,
-    //   {
-    //     amount: webhookEventDto.data.attributes.data.attributes.amount,
-    //     currencyId: 'php',
-    //     fee:
-    //       webhookEventDto.data.attributes.data.attributes.fee +
-    //       webhookEventDto.data.attributes.data.attributes.foreign_fee,
-    //     paymentServiceId: 'paymongo',
-    //     status: FundTransferStatus.SUCCESS,
-    //     type: FundTransferType.EXTERNAL,
-    //     userId: user.id,
-    //   },
-    // );
-
-    // return externalFundTransfer;
+    return externalFundTransfer;
   }
 }
