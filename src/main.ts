12145b672217f1563c8578b518c137920d27c1ae
Ralf <ralflopez3@gmail.com>
2022-07-12 17:30:50 +0800

    :lock: add domain in express session cookie

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const urlObject = new URL(process.env.BASE_URL || 'http://localhost');
+  const hostName = urlObject.hostname;
+  const domainName = hostName.replace(/^[^.]+\./g, '');
+
   // express session
   app.use(
     session({
+      cookie: {
+        domain: domainName,
+      },
       secret: 'secret',
       resave: false,
       saveUninitialized: false,
